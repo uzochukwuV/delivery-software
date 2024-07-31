c76725ac54b68e7b7dd87bdf745a9ade1c27a17c
uuzor <victor.u.ezealor@gmail.com>
2024-07-31 12:14:29 +0200

    feat: added vercel.json and rewrites all path

--- src/components/landing/Footer.tsx
+++ src/components/landing/Footer.tsx
@@ -1,4 +1,5 @@
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { SITE_NAME } from "../../constants/names";
 
 
 function Footer() {
@@ -6,7 +7,9 @@ function Footer() {
   return (
     <div className=" max-w-[1400px] md:py-16 mx-auto sm:px-10 md:px-16">
       <div className=" h-[150px] px-6 md:px-0 border-b flex flex-col justify-center gap-4">
-        <h2>Logo</h2>
+      <h2 className=" text-slate-700 font-logo text-3xl m-0 font-extrabold leading-3 text-justify">
+            <Link to={'/'} className=" text-white"> <span className=" text-slate-700">{SITE_NAME}</span> </Link>
+          </h2>
        <div>
        <div className="text-[9px] font-semibold logo-bottom mb-2">
           ISO 9001: 2015, ISO 27001: <span onClick={()=> navigate("/main/admin")}>2013</span> Certified Company
@@ -21,7 +24,7 @@ function Footer() {
           Information Security Policy
         </div> 
         <div  className="text-sm leading-relaxed">
-          Delhivery is committed to safeguarding the confidentiality,
+        {SITE_NAME} is committed to safeguarding the confidentiality,
           integrity and availability of all physical and electronic
           information assets of the organization.<br  />
           We ensure that the regulatory, operational and contractual
